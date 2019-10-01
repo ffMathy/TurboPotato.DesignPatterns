f2e41856a9bac7cdb8e2eeaaf7b7f56f02dd3610
Mathias Lorenzen <mathias.lorenzen@live.com>
2019-10-01 22:14:28 +0200

    strategy pattern implemented.

---
+++ src/TurboPotato.DesignPatterns.Strategy/Strategies/LowHealthStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TurboPotato.DesignPatterns.Strategy.Strategies
+{
+    class LowHealthStrategy : IStrategy
+    {
+        public void Act(Player player)
+        {
+            Console.WriteLine("Oh noes, we have low health");
+
+            //TODO: move away from enemy if an enemy is too close.
+            //TODO: try to heal if enemies are far away.
+        }
+    }
+}
