81fdf4aedfe27f476ba5e26a47aa3e8f60e91714
Mathias Lorenzen <mathias.lorenzen@live.com>
2019-10-01 22:06:57 +0200

    added a bit to strategy pattern.

---
+++ src/TurboPotato.DesignPatterns.Strategy/Strategies/IStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TurboPotato.DesignPatterns.Strategy.Strategies
+{
+    interface IStrategy
+    {
+        void Act();
+    }
+}
