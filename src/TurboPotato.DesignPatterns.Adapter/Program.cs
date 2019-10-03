72192e83e964710498c01e51bb3a0b601720f4c0
Mathias Lorenzen <mathias.lorenzen@live.com>
2019-10-03 22:41:31 +0200

    adapter pattern.

--- src/TurboPotato.DesignPatterns.Adapter/Program.cs
+++ src/TurboPotato.DesignPatterns.Adapter/Program.cs
@@ -2,11 +2,61 @@
 
 namespace TurboPotato.DesignPatterns.Adapter
 {
+    public interface IMicroUsbPhone
+    {
+        string RechargeUsingMicroUsb();
+    }
+
+    public interface ILightningPhone
+    {
+        string RechargeUsingLightning();
+    }
+
+    class IPhone : ILightningPhone
+    {
+        public string RechargeUsingLightning()
+        {
+            return "Recharge using lightning";
+        }
+    }
+
+    class Android : IMicroUsbPhone
+    {
+        public string RechargeUsingMicroUsb()
+        {
+            return "Recharge using Micro USB";
+        }
+    }
+
+    // The Adapter makes the Adaptee's interface compatible with the Target's
+    // interface.
+    class LightningToMicroUsbAdapter : IMicroUsbPhone
+    {
+        private readonly ILightningPhone _lightningPhone;
+
+        public LightningToMicroUsbAdapter(ILightningPhone lightningPhone)
+        {
+            this._lightningPhone = lightningPhone;
+        }
+
+        public string RechargeUsingMicroUsb()
+        {
+            return _lightningPhone.RechargeUsingLightning();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var android = new Android();
+            Console.WriteLine(android.RechargeUsingMicroUsb());
+
+            var iphone = new IPhone();
+            Console.WriteLine(iphone.RechargeUsingLightning());
+
+            var adapter = new LightningToMicroUsbAdapter(iphone);
+            Console.WriteLine(adapter.RechargeUsingMicroUsb());
         }
     }
 }
